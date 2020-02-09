e09d8c10cf73b63ef627b6d4cf7528e6aef36cb3
Szymon Dudziak <sdudziak@users.noreply.github.com>
2020-02-09 23:17:02 +0100

    Fixed integration tests coverage report

---
+++ core/src/integration-test/kotlin/io/saas/starter/ApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.saas.starter
+
+import io.saas.starter.modules.security.validation.MatchingFieldsConstraintValidator
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [MatchingFieldsConstraintValidator::class])
+@Tag("integrationTest")
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
+
